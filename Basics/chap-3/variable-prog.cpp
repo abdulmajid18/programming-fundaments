46e215995884fe2ce26f322574abbc1cb55dd833
abdulmajid18 <nuhuabdulmajid58@gmail.com>
2023-03-25 14:23:13 +0000

    variable definition

---
+++ Basics/chap-3/variable-prog.cpp
@@ -0,0 +1,9 @@
+#include <string>
+int main()
+{
+    int number_of_steps = 39;
+    double flying_time = 3.5;
+    char decimal_point = '.';
+    std::string name = "Annemarie";
+    bool tap_on = true;
+}
