12cfd904452950b8b0079f5e73d564a7cb6c1b99
abdulmajid18 <nuhuabdulmajid58@gmail.com>
2023-03-25 13:14:18 +0000

    print hello world and use header file

---
+++ Basics/chap-2/drill/hello_world.cpp
@@ -0,0 +1,8 @@
+#include "std_lib_facilities.h"
+
+int main()
+{
+    std::cout << "Hello. World!\n"; // outpujt "Hello,World"
+    keep_window_open();             // wait for a character to be entered
+    return 0;
+}
