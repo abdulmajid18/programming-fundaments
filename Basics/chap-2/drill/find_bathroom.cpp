8dbaf001a8ed7f5057dcf8b61fb5c63314bd759b
abdulmajid18 <nuhuabdulmajid58@gmail.com>
2023-03-25 13:37:48 +0000

    basic finding bathroom prog

---
+++ Basics/chap-2/drill/find_bathroom.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Open the main entrance\n";
+    std::cout << "Push round buttom\n";
+    std::cout << "Move 10 steps \n";
+    std::cout << "Turn right\n";
+    std::cout << "Move 10 steps\n";
+    std::cout << "Turn left\n";
+    std::cout << "Move 15 steps up\n";
+    std::cout << "Turn left\n";
+    std::cout << "Move 20 steps right up\n";
+    std::cout << "Turn left\n";
+    std::cout << "Move 20 steps left up\n";
+    std::cout << "Turn right\n";
+    std::cout << "Move 15 steps\n";
+    std::cout << "Turn right\n";
+    std::cout << "Move 32 steps\n";
+    std::cout << "Turn right\n";
+    std::cout << "Move 15 steps\n";
+    std::cout << "Push round buttom\n";
+}
