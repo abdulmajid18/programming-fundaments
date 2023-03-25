d770a4f14dc06fbaca5b067e457c181bd2dccd1e
abdulmajid18 <nuhuabdulmajid58@gmail.com>
2023-03-25 13:21:58 +0000

    exercise: 2 change output text

--- Basics/chap-2/drill/hello_world.cpp
+++ Basics/chap-2/drill/hello_world.cpp
@@ -2,7 +2,8 @@
 
 int main()
 {
-    std::cout << "Hello. World!\n"; // outpujt "Hello,World"
-    keep_window_open();             // wait for a character to be entered
+    std::cout << "Hello, progrmming!\n"; // outpujt "Hello,World"
+    std::cout << "Here we go!\n";
+    keep_window_open(); // wait for a character to be entered
     return 0;
 }
