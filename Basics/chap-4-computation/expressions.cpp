7050a6114dc13c393a387fd05da18eb4c3b6aeb4
abdulmajid18 <nuhuabdulmajid58@gmail.com>
2023-04-01 09:07:29 +0000

    expression

---
+++ Basics/chap-4-computation/expressions.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int length = 20;
+    int width = 40;
+    int area = length * width;
+
+    cout << "Area " << area << " Out put \n";
+    area = 2;
+    cout << "The new value is  " << area << endl;
+}
